1c762532d73eb8348f6131e7db9b2d2560104046
kei.effie@gmail.com <kei.effie@gmail.com>
2019-09-21 11:11:27 +0300

    Some Comments

--- SockMerchant/Program.cs
+++ SockMerchant/Program.cs
@@ -3,13 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SockMerchant
 {
     class Program
     {
+        // Complete the sockMerchant function below.
+        static int SockMerchant(int n, int[] ar)
+        {
+            int temp = 0;
+            int count = 1;
+            int totalCount = 0;
+           if (n >= 1 && n <= 100)
+            {
+                Array.Sort(ar, 0, n);
+
+
+                Console.WriteLine("***********");
+                foreach (int ii in ar)
+                {
+
+                    Console.Write(ii);
+                    Console.Write(" ");
+                }
+                Console.WriteLine("***********");
+
+
+                for (int i = 0; i < n; i++)
+                {
+                    count = 1;
+                    if (temp != ar[i])
+                    {
+                        for (int j = i + 1; j < ar.Length; j++)
+                        {
+                            //temp = ar[i+1];
+                            if (ar[i] == ar[j])
+                            {
+                                count += 1;   
+                                temp = ar[i];
+                            }
+                        }
+
+                        if (count % 2 == 0)
+                        {
+                            totalCount += count / 2;
+                        }
+                        else if (count % 2 != 0 && count > 2)       // if count = 1 ==>> initialization no use here
+                        {
+                            totalCount += (count - 1) / 2;
+                        }
+                    }
+                }
+            }
+            return totalCount;
+        }
+
         static void Main(string[] args)
         {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+           // int[] ar = new int[] { 50, 20, 20, 10, 30, 30, 50, 10, 20, 14, 14 };
+            int result = SockMerchant(n, ar);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
         }
     }
+
 }
+
